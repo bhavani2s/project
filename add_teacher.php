7b0c761e78b3a6cbfb6676609e432f117d87ceb0
bhavani2s <bhavani.202021@gmail.com>
2025-04-12 15:06:08 +0530

    Create add_teacher.php

---
+++ add_teacher.php
@@ -0,0 +1,76 @@
+<?php
+// add_teacher.php (Modified for teacher_id)
+session_start();
+
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    header("Location: admin_login.php");
+    exit;
+}
+
+$servername = "localhost";
+$username_db = "root";
+$password_db = "";
+$dbname = "sai";
+
+$conn = new mysqli($servername, $username_db, $password_db, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$error_message = "";
+$success_message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $teacher_id = $_POST["teacher_id"];
+    $password = $_POST["password"];
+
+    $teacher_id = mysqli_real_escape_string($conn, $teacher_id);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    $sql = "INSERT INTO teachers (teacher_id, password) VALUES ('$teacher_id', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        $success_message = "Teacher added successfully.";
+    } else {
+        $error_message = "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Teacher</title>
+</head>
+<body>
+    <div class="navbar">
+        <a href="admin_dashboard.php">Home</a>
+        <a href="add_student.php">Add Student</a>
+        <a href="add_teacher.php">Add Teacher</a>
+        <a href="manage_admins.php">Admins</a>
+    </div>
+
+    <h2>Add Teacher</h2>
+
+    <?php if (!empty($error_message)) { ?>
+        <p style="color: red;"><?php echo $error_message; ?></p>
+    <?php } ?>
+
+    <?php if (!empty($success_message)) { ?>
+        <p style="color: green;"><?php echo $success_message; ?></p>
+    <?php } ?>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="teacher_id">Teacher ID:</label>
+        <input type="text" name="teacher_id" id="teacher_id" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required><br><br>
+
+        <input type="submit" value="Add Teacher">
+    </form>
+</body>
+</html>

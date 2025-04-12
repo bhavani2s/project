e1505060b87101c821aca1890ae76ee3908391f9
bhavani2s <bhavani.202021@gmail.com>
2025-04-12 15:11:33 +0530

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -1,32 +1,69 @@
 <?php
-// admin_dashboard.php
+// admin_login.php
+$servername = "localhost";
+$username_db = "root";
+$password_db = "";
+$dbname = "sai";
+
+$conn = new mysqli($servername, $username_db, $password_db, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 session_start();
 
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header("Location: admin_login.php");
+if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true) {
+    header("Location: admin_dashboard.php");
     exit;
 }
 
-$admin_username = $_SESSION['admin_username'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $username = mysqli_real_escape_string($conn, $username);
+    $sql = "SELECT * FROM admins WHERE username = '$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        if ($password === $row["password"]) {
+            $_SESSION['admin_logged_in'] = true;
+            $_SESSION['admin_username'] = $username;
+            header("Location: admin_dashboard.php");
+            exit;
+        } else {
+            $error_message = "Incorrect password.";
+        }
+    } else {
+        $error_message = "Incorrect username.";
+    }
+}
+
+$conn->close();
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Admin Login</title>
 </head>
 <body>
-<h2>Welcome, <?php echo $admin_username; ?>!</h2>
-    <div class="navbar">
-        <a href="admin_dashboard.php">Home</a>
-        <a href="add_student.php">Add Student</a>
-        <a href="add_teacher.php">Add Teacher</a>
-        <a href="manage_admins.php">Admins</a>
-    </div>
+    <h2>Admin Login</h2>
 
-    <button>
-        <a href="logout.php">Logout</a>
-    </button>
+    <?php if (isset($error_message)) { ?>
+        <p style="color: red;"><?php echo $error_message; ?></p>
+    <?php } ?>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required><br><br>
+
+        <input type="submit" value="Login">
+    </form>
 </body>
 </html>

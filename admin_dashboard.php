3f4c63216f7b5e28a275b6a3b0b0711314ce738d
bhavani2s <bhavani.202021@gmail.com>
2025-05-01 14:05:54 +0530

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,5 +1,5 @@
 <?php
-// admin_dashboard.php
+
 session_start();
 
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
@@ -7,23 +7,18 @@ if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== tru
     exit;
 }
 
-$admin_username = $_SESSION['admin_username'];
+$admin_id = $_SESSION['admin_id'];
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h2>Welcome, <?php echo $admin_username; ?>!</h2>
-    <div class="navbar">
-        <a href="admin_dashboard.php">Home</a>
-        <a href="add_student.php">Add Student</a>
-        <a href="add_teacher.php">Add Teacher</a>
-        <a href="manage_admins.php">Admins</a>
-    </div>
+    <h2>Welcome, <?php echo $admin_id; ?>!</h2>
+    
+    <h4>Admin Dashboard</h4>
 
     <button>
         <a href="logout.php">Logout</a>
